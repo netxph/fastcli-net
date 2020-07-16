efae3db3cab8e21019c78d81b2835a3e9bbf1b02
VITALIS, Marc <marcabraham.vitalis@amadeus.com>
2020-07-16 18:40:43 +0800

    first checkin

---
+++ src/FastCli.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastCli.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FastCli! Let's Go!");
+        }
+    }
+}
