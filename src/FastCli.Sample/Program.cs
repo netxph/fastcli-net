c43d8c4d28886e9c4346ef6e3319b44a06837868
Marc Vitalis <marc.vitalis@live.com>
2020-07-16 19:48:36 +0800

    target setup

--- src/FastCli.Sample/Program.cs
+++ src/FastCli.Sample/Program.cs
@@ -4,9 +4,35 @@ namespace FastCli.Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("FastCli! Let's Go!");
+            Console.WriteLine("Let's Go! FastCli");
+
+            await new MainHost()
+                .Use(new JsonConfiguration("local.appsettings.json"))
+                .Use(new EnvironmentConfiguration("CLI_"))
+                .Use(new SecretConfiguration())
+                .Use(new ArgumentConfiguration(args))
+                .StartAsync()
         }
+    }
+
+    public class MainHost : CliHost
+    {
+        protected override void OnRegister(CommandBuilder builder)
+        {
+            builder
+                .Register<SampleCommand>()
+                    .WithSubCommand<HelloCommand>()
+                    .WithSubCommand<WorldCommand>()
+                .Register<MessengerCommand>()
+                    .WithSubCommand<SenderCommand>();
+        }
+
+        protected override void OnConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<ILogger, Logger>();
+        }
+
     }
 }
